5834b187dfc5424e23aa8a5964cee13a66019fae
harindr404 <harindra.saitej@gmail.com>
2017-02-16 09:45:19 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,24 +1,25 @@
 import urllib
-
 from PIL import image
 import pytesseract
 from resizeimage import resizeimage
 import os
 import cv2
 import numpy as np
-src_path="F:/Temp?"
+src_path="C:\Users\harindra sai tej\PycharmProjects\untitled"
+img_path="C:\Users\harindra sai tej\PycharmProjects\untitled\A-wise-man-can-learn"
 
-def get_string(img_path);
+
+def get_string(img_path):
 img=cv2.imread(img_path)
 img=cv2.cvtColor(img, cv2,COLOR_BGR2GRAY)
 kernel=np.ones((1,1), np.uint8)
 img= cv2.dilate(img, kernel, iterations=1)
 img= cv2.erode(img, kernel, iterations=1)
 
-cv2.imwrite(src_path + "removed_noise.png:, img)
-img= cv2.adaptiveThreshold(img, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY, 11, 2)
-cv2.imwrite(src_path+ "thres.png", img)
-result=pytesseract.image to string(image.open(src path +"thres.png"))
+cv2.imwrite(src_path + "A-wise-man-can-learn.png", img)
+img= cv2.adaptiveThreshold(img, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.AWISEMANCANLEARN.png_BINARY, 11, 2)
+cv2.imwrite(src_path+ "A-wise-man-can-learn.png.png", img)
+result=pytesseract.image to string(image.open(src path +"A-wise-man-can-learn.png"))
 
 return result
 
